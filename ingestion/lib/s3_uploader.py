5bffc2369613790f233a3daa2fa701ac8733af3c
Alec Ryan <alecryan729@gmail.com>
2025-12-16 16:23:31 -0500

    Add logging

--- ingestion/lib/s3_uploader.py
+++ ingestion/lib/s3_uploader.py
@@ -1,6 +1,10 @@
-import boto3
 import json
+import logging
 import os
+
+import boto3
+
+logger = logging.getLogger(__name__)
 
 
 class S3Uploader:
@@ -9,8 +13,15 @@ class S3Uploader:
         self.bucket = os.environ['S3_BUCKET']
 
     def upload_game_data(self, game_object: dict, partition_date: str):
+        game_id = game_object['id']
+        key = f"game_data={partition_date}/game_id={game_id}.json"
+        
+        logger.info(f"Uploading game to S3: bucket={self.bucket}, key={key}")
+        
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"game_data={partition_date}/game_id={game_object['id']}.json",
+            Key=key,
             Body=json.dumps(game_object)
         )
+        
+        logger.info(f"Successfully uploaded game to S3: game_id={game_id}")
