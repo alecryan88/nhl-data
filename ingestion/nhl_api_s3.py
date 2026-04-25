42385e9bedb321001fe119788ca706b954504f01
Alec Ryan <alecryan729@gmail.com>
2026-04-25 14:52:19 -0400

    add

--- ingestion/nhl_api_s3.py
+++ ingestion/nhl_api_s3.py
@@ -10,32 +10,32 @@ logger.setLevel(logging.INFO)
 
 
 def main(event: dict):
-    logger.info(f"Processing event: {event}")
+    logger.info(f'Processing event: {event}')
 
     # Get the date of the event
     base = datetime.datetime.fromisoformat(event['time'].replace('Z', '+00:00')).date()
     yesterday = base - datetime.timedelta(days=2)
     yesterday_str = yesterday.strftime('%Y-%m-%d')
 
-    logger.info(f"Fetching games for date: {yesterday_str}")
+    logger.info(f'Fetching games for date: {yesterday_str}')
 
     # Get the list of games objects for the date
     nhl_api = NHLAPIHandler()
     nhl_games = nhl_api.get_daily_games(yesterday_str)
 
-    logger.info(f"Found {len(nhl_games)} games to upload")
+    logger.info(f'Found {len(nhl_games)} games to upload')
 
     # Upload each game object to S3
     s3_uploader = S3Uploader()
     for game in nhl_games:
         s3_uploader.upload_game_data(game, yesterday_str)
 
-    logger.info(f"Completed uploading {len(nhl_games)} games to S3")
+    logger.info(f'Completed uploading {len(nhl_games)} games to S3')
 
 
 def lambda_handler(event, context) -> dict:
-    logger.info("Lambda invoked")
+    logger.info('Lambda invoked')
 
     main(event)
 
-    return {"statusCode": 200, "body": "Success"}
+    return {'statusCode': 200, 'body': 'Success'}
